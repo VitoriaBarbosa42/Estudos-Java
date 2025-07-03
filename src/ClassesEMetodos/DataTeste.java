8d263aef6147e31494c751e2ed85da71d7980d7b
VitoriaBarbosa42 <vitoria42.barbosa@gmail.com>
2025-07-02 23:30:32 -0300

    Atualização do inicio da sessão classe e metodos do curso java

--- src/ClassesEMetodos/DataTeste.java
+++ src/ClassesEMetodos/DataTeste.java
@@ -6,13 +6,13 @@ public class DataTeste {
 		
 		ClasseData dia1 = new ClasseData();
 			dia1.dia = 5;
-			dia1.mes = "junho";
+			dia1.mes = 05;
 			dia1.ano = 2025;
 			
 			
 		ClasseData dia2 = new ClasseData();
 			dia2.dia = 15;
-			dia2.mes = "Janeiro";
+			dia2.mes = 01;
 			dia2.ano = 2000;
 			
 
