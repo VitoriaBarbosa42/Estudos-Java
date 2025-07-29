f404be9cad9edc2865247973818b24843543ce76
VitoriaBarbosa42 <vitoria42.barbosa@gmail.com>
2025-07-29 17:41:51 -0300

    Finalizando desafio da sessão lambda

---
+++ src/lambda/Desafio.java
@@ -0,0 +1,37 @@
+package lambda;
+
+import java.util.function.BinaryOperator;
+import java.util.function.UnaryOperator;
+
+public class Desafio {
+	
+	public static void main(String[] args) {
+		Produto p = new Produto("ipad", 3235.89, 0.13);
+		
+		BinaryOperator<Double> precoComDesconto = 
+				(precoBruto, porcentagemDeDesconto) -> 
+				precoBruto * (1 - porcentagemDeDesconto);
+		
+		UnaryOperator<Double> valorImpostoMunicipal = preco -> preco > 2500 ? preco += (preco * 0.085) : preco;
+		
+		UnaryOperator<Double> frete1 = preco -> preco >= 3000.00 ? preco += 100 : preco;
+		UnaryOperator<Double> frete2 = preco -> preco < 3000.00 ? preco += 50 : preco;
+		
+		
+		
+		Double resultado = precoComDesconto.andThen(valorImpostoMunicipal).andThen(frete1).andThen(frete2).apply(p.preco, p.desconto);
+		
+		System.out.printf("Valor final do produto: %.2f R$", resultado);
+	}
+}
+/*
+	 * 1. A partir do produto calcule o preço real (com desconto)
+	 * 2. Imposto Municipal: >= 2500 (8,5%) / < 2500 (isento)
+	 * 3. Frete: >= 3000 (100) / < 3000 (50)
+	 * 4. Arredondar: Deixar duas casas decimais
+	 * 5. Formatar: R$1234,56
+	 */
+//		BiFunction<Double, Double, Double> impostoMunicipal = (valorBruto, porcentagemImposto) -> {
+//			boolean condicao = valorBruto < 2500.00;
+//			return condicao ? valorBruto * 0.0 : valorBruto * 0.085;
+//		};
